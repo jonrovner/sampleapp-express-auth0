ff0decb03e81ee59268e86c6c02c519d77ba06fe
jonrovner <jonrovner@gmail.com>
2022-05-10 09:40:51 -0300

    updated auth router for deploy

--- routes/auth.js
+++ routes/auth.js
@@ -43,7 +43,7 @@ router.post('/logout', function(req, res, next) {
   req.logout();
   var params = {
     client_id: process.env['AUTH0_CLIENT_ID'],
-    returnTo: 'http://localhost:3000/'
+    returnTo: 'https://charliessampleapplication.herokuapp.com/'
   };
   res.redirect('https://' + process.env['AUTH0_DOMAIN'] + '/v2/logout?' + qs.stringify(params));
 });
